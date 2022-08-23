1ce2a4ac5c89ba2e0ec0097e681b65f9e75c14ab
Kirill <pilot-stalker@mail.ru>
2022-08-23 12:41:37 +0300

    5 zdacha

---
+++ hometask5/Program.cs
@@ -0,0 +1,12 @@
+﻿//Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+for (int x = num*-1; x < num + 1; x++)       
+    if (x<num+1)
+    {
+        Console.Write(x);
+        Console.Write(". " );
+
+    }
